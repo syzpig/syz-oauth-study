fa3dcf99ba4e57cb9a63d73e1a1d9a6c4f6b0a0a
shaoyingzhu <yingzhu.shao@newtouch.cn>
2018-05-29 14:03:27 +0800

    添加admin-zuul模块与mybatis代码自动那个生成

---
+++ admin-user/src/main/java/com/syz/security/user/mapper/BaseUserMapper.java
@@ -0,0 +1,7 @@
+package com.syz.security.user.mapper;
+
+import com.syz.security.user.entity.BaseUser;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface BaseUserMapper extends Mapper<BaseUser> {
+}
---
+++ admin-zuul/src/main/java/com/syz/security/zuul/AdminZuulApplication.java
@@ -0,0 +1,12 @@
+package com.syz.security.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminZuulApplication.class, args);
+	}
+}
