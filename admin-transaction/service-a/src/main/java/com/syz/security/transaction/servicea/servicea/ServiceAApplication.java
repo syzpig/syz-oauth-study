10217bec10b87fe70ca6b0c0374a5b67cef4b17f
shaoyingzhu <yingzhu.shao@newtouch.cn>
2019-03-07 18:38:15 +0800

    添加分布式事务基础模块，并实现手动获取spring实现的connection,来实现控制本地事务代码

---
+++ admin-transaction/service-a/src/main/java/com/syz/security/transaction/servicea/servicea/ServiceAApplication.java
@@ -0,0 +1,13 @@
+package com.syz.security.transaction.servicea.servicea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceAApplication.class, args);
+	}
+
+}
